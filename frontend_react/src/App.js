600995690effe00b42fd38cc032821c4bb201a8f
Victoriia-Ripka <vikaripka@gmail.com>
2023-01-18 20:31:11 +0100

    images

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -2,9 +2,10 @@
 import React from "react";
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { NavBar } from "./components";
+import './App.scss'
 
 const App = () => {
-    return <div className="">
+    return <div className="app">
         <NavBar/>
         <Header />
         <About />
--- frontend_react/src/components/NavBar/NavBar.jsx
+++ frontend_react/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import './NavBar.scss'
+import React from "react";
+import "./NavBar.scss";
+import { images } from "../../constants";
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+  const menu = ["home", "about", "work", "skills", "contact"];
 
-export default NavBar
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {menu.map((item) => {
+          return (
+            <li key={`link-${item}`} className="app__flex p-text">
+              <div>
+                <a href={`#${item}`}>{item}</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
